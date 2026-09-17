c8567de901509a08f8d30c72c04d391f239a9df1
Hassanmsaid <hassanmsaid@hotmail.com>
2026-09-17 16:02:12 +0400

    Added Exercise model

---
+++ lib/models/exercise.dart
@@ -0,0 +1,41 @@
+enum TargetType { reps, time }
+
+class Exercise {
+  final String id;
+  String name;
+  String category;
+  int targetSets;
+  TargetType targetType;
+  bool isCompleted;
+
+  Exercise({
+    required this.id,
+    required this.name,
+    required this.category,
+    required this.targetSets,
+    required this.targetType,
+    this.isCompleted = false,
+  });
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'category': category,
+      'targetSets': targetSets,
+      'targetType': targetType.name,
+      'isCompleted': isCompleted,
+    };
+  }
+
+  factory Exercise.fromJson(Map<String, dynamic> json) {
+    return Exercise(
+      id: json['id'],
+      name: json['name'],
+      category: json['category'],
+      targetSets: json['targetSets'],
+      targetType: TargetType.values[json['targetType']],
+      isCompleted: json['isCompleted'],
+    );
+  }
+}
